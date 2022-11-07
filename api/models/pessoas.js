645c7531995ad20c28c79bd1204943293f16714c
Kaique D Felipe <83593559+kaique-kira@users.noreply.github.com>
2022-11-07 18:52:19 -0300

    add: validações

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -1,9 +1,13 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Pessoas = sequelize.define('Pessoas', {
-    nome: DataTypes.STRING,
+    nome:{ type: DataTypes.STRING, validate: {funcaoValidadora: function(dado){
+      if(dado.length < 3) throw new Error('O campo nome deve ter mais de 3 caracteres')
+    }}},
     ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
+    email:{ type: DataTypes.STRING, validate: {isEmail: {
+      args: true, msg: 'dados do tipo e-mail inválido'
+    }}},
     role: DataTypes.STRING
   }, {
     paranoid: true,
